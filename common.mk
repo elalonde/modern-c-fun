4ba9d5bd1709d26b733c27c24ab03ce3a0250b0a
Eric Lalonde <eric@lalonde.com>
2026-09-10 15:28:37 -0700

    Support target-specific build layout
    - also get rid of compile_commands.json file generation as clang is out
      of scope.

--- common.mk
+++ common.mk
@@ -1,4 +1,6 @@
-BUILD_DIR = ../build/$(BIN)
+BUILD_ROOT = ../build
+BIN_ROOT = $(BUILD_ROOT)/$(BIN)
+BUILD_DIR = $(BIN_ROOT)/$(TARGET)
 BIN_TARGET = $(BUILD_DIR)/$(BIN)
 SRC_DIR = .
 SRCS = $(SRC_DIR)/$(BIN).c
@@ -13,10 +15,15 @@ DEPFLAGS = -MMD -MP
 CC = gcc
 LD = ld
 LDFLAGS =
-BIN_TARGET_ARGS =
 
 ifeq ($(WERROR), 1)
 CWARNINGS := $(CWARNINGS) -Werror
+endif
+
+ifeq ($(MAKECMDGOALS),)
+TARGET=debug
+else
+TARGET=$(MAKECMDGOALS)
 endif
 
 hardened: CFLAGS = -O2 -g -D_FORTIFY_SOURCE=3  -fstack-protector-strong \
@@ -25,28 +32,30 @@ hardened: CFLAGS = -O2 -g -D_FORTIFY_SOURCE=3  -fstack-protector-strong \
 
 analyze: CFLAGS = -O2 -fanalyzer
 
-.PHONY: debug hardened analyze clean
+.PHONY: debug hardened analyze clean spotless
 
-$(info BIN  = [$(BIN)])
-$(info SRCS = [$(SRCS)])
-$(info OBJS = [$(OBJS)])
+#$(info BIN  = [$(BIN)])
+#$(info SRCS = [$(SRCS)])
+#$(info OBJS = [$(OBJS)])
 
 debug: $(BIN_TARGET)
 
 hardened: $(BIN_TARGET)
 
-analyze: | $(BIN_TARGET)
-	clang-tidy $(SRCS)
+analyze: $(BIN_TARGET)
 
-test:
 clean:
-	rm -rf ../build/$(BIN)
+	rm -rf --one-file-system --preserve-root $(BIN_ROOT)
+
+spotless:
+	rm -rf --one-file-system --preserve-root $(BUILD_ROOT)
+
 
 $(BIN_TARGET): $(OBJS)
-	$(CC) $(CFLAGS) $(CWARNINGS) $(OBJS) -o $@
+	$(CC) $(LANG_STD) $(CFLAGS) $(CWARNINGS) $(OBJS) -o $@
 
 $(BUILD_DIR)/%.o: $(SRC_DIR)/%.c | $(BUILD_DIR)
-	$(CC) $(CFLAGS) $(DEPFLAGS) $(CWARNINGS) -c $< -o $@
+	$(CC) $(LANG_STD) $(CFLAGS) $(DEPFLAGS) $(CWARNINGS) -c $< -o $@
 
 $(BUILD_DIR):
 	mkdir -p $@
